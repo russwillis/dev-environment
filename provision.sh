176bfed6a9b08d0a97b5a5950035ac2020ece451
RussW <russw@LRs-MacBook-Pro-2.local>
2015-01-27 14:00:34 +0000

    First pass at ubuntu box and postgres

---
+++ provision.sh
@@ -0,0 +1,7 @@
+# echo "Install Python PIP"
+# apt-get -y install python-pip python-dev build-essential
+# pip install --upgrade virtualenv
+
+
+# install Postgres DB
+apt-get -y install postgresql postgresql-contrib
